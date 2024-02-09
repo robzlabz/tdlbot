9cc9b937e9d3c59b80a4263d4b6261381e6d20ba
Robbyn Rahmandaru <robzlabz@gmail.com>
2024-02-10 04:39:24 +0700

    initial

---
+++ index.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import TelegramBot from 'node-telegram-bot-api';
+
+dotenv.config();
+const token = process.env.TELEGRAM_BOT_TOKEN || '';
+const igDownloadUrl = process.env.IG_DOWNLOAD_URL;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    console.log('Received a message:', text);
+
+    const instagramUrl = text.match(/https:\/\/www\.instagram\.com\/(p|reel)\/\w+\//);
+    if (instagramUrl) {
+        console.log('Instagram URL detected:', instagramUrl[0]);
+
+        try {
+            const response = await axios.get(`${igDownloadUrl}/api/video?url=${instagramUrl[0]}`);
+            const videoUrl = response.data.data.videoUrl;
+            await bot.sendMessage(chatId, 'Received video URL: ' + videoUrl);
+
+            await bot.sendMessage(chatId, 'Downloading video...');
+            const res = await fetch(videoUrl);
+            const fileStream = fs.createWriteStream('temp.mp4');
+            await new Promise((resolve, reject) => {
+                res.body.pipe(fileStream);
+                res.body.on("error", (err) => {
+                    reject(err);
+                });
+                fileStream.on("finish", function () {
+                    resolve();
+                });
+            });
+            await bot.sendMessage(chatId, 'Sending video...');
+            await bot.sendVideo(chatId, 'temp.mp4');
+
+            fs.unlinkSync('temp.mp4');
+        } catch (error) {
+            console.error('An error occurred:', error);
+            bot.sendMessage(chatId, 'An error occurred while processing your request.');
+        }
+    } else {
+        console.log('No Instagram URL detected in message.');
+        bot.sendMessage(chatId, 'Please enter the Instagram link you want to download. For example, https://www.instagram.com/p/BvQjgKClbv_/');
+    }
+});
